c2735adc19f82dff7803d94184834257cf6c47e2
2Cubed <2Cubed@users.noreply.github.com>
2017-08-13 20:27:43 -0400

    Fix emoji conversions

--- src/service/services/mixer/emoji.ts
+++ src/service/services/mixer/emoji.ts
@@ -1,157 +1,483 @@
-export const emojis: Emojis = {
-    ":spaceship": "rocket",
-    ":127": "fire",
-    ":cactus": "cactus",
-    ":whoappa": "astonished",
-    ":bacon": "",
-    ":beer": "beer",
-    ":cake": "cake",
-    ":candy": "candy",
-    ":fries": "fries",
-    ":icecream": "hot_pepper",
-    ":pizza": "pizza",
-    ":popcorn": "popcorn",
-    ":turqmelon": "watermelon",
-    ":afk": "door",
-    ":controller": "video_game",
-    ":delorean": "blue_car",
-    ":facepalm": "",
-    ":rmf": "desktop_computer",
-    ":swag": "sunglasses",
-    ":wrench": "wrench",
-    ":christmas": "christmas_tree",
-    ":jinglebell": "bell",
-    "<3": "",
-    "</3": "broken_heart",
-    ":')": "joy",
-    ":'-)": "joy",
-    ":D": "smiley",
-    ":-D": "smiley",
-    "=D": "smiley",
-    ":)": "smile",
-    ":-)": "smile",
-    "=]": "smile",
-    "=)": "smile",
-    ":]": "smile",
-    "\":)": "sweat_smile",
-    "\":-)": "sweat_smile",
-    "'=)": "sweat_smile",
-    "':D": "sweat_smile",
-    "':-D": "sweat_smile",
-    "'=D": "sweat_smile",
-    ">:)": "satisfied",
-    ">;)": "satisfied",
-    ">:-)": "satisfied",
-    ">=)": "satisfied",
-    ";)": "wink",
-    ";-)": "wink",
-    "*-)": "wink",
-    "*)": "wink",
-    ";-]": "wink",
-    ";]": "wink",
-    ";^)": "wink",
-    "':(": "sweat",
-    "':-(": "sweat",
-    "'=(": "sweat",
-    ":*": "kissing_heart",
-    ":-*": "kissing_heart",
-    "=*": "kissing_heart",
-    ":^*": "kissing_heart",
-    ">:P": "stuck_out_tongue_winking_eye",
-    "X-P": "stuck_out_tongue_winking_eye",
-    "x-p": "stuck_out_tongue_winking_eye",
-    ">:[": "disappointed",
-    ":-(": "disappointed",
-    ":(": "disappointed",
-    ":-[": "disappointed",
-    ":[": "disappointed",
-    "=(": "disappointed",
-    ">:(": "angry",
-    ">:-(": "angry",
-    ":@": "angry",
-    ":'(": "cry",
-    ":'-(": "cry",
-    ";(": "cry",
-    ";-(": "cry",
-    ">.<": "persevere",
-    ":$": "flushed",
-    "=$": "flushed",
-    "#-)": "dizzy_face",
-    "#)": "dizzy_face",
-    "%-)": "dizzy_face",
-    "%)": "dizzy_face",
-    "X)": "dizzy_face",
-    "X-)": "dizzy_face",
-    "*\\0/*": "ok_woman",
-    "\\0/": "ok_woman",
-    "*\\O/*": "ok_woman",
-    "\\O/": "ok_woman",
-    "O:-)": "innocent",
-    "0:-3": "innocent",
-    "0:3": "innocent",
-    "0:-)": "innocent",
-    "0:)": "innocent",
-    "0;^)": "innocent",
-    "O:)": "innocent",
-    "O;-)": "innocent",
-    "O=)": "innocent",
-    "0;-)": "innocent",
-    "O:-3": "innocent",
-    "O:3": "innocent",
-    "B-)": "sunglasses",
-    "B)": "sunglasses",
-    "8)": "sunglasses",
-    "8-)": "sunglasses",
-    "B-D": "sunglasses",
-    "8-D": "sunglasses",
-    "-_-": "expressionless",
-    "-__-": "expressionless",
-    "-___-": "expressionless",
-    ">:\\": "confused",
-    ">:/": "confused",
-    ":-/": "confused",
-    ":-.": "confused",
-    ":/": "confused",
-    ":\\": "confused",
-    "=/": "confused",
-    "=\\": "confused",
-    ":L": "confused",
-    "=L": "confused",
-    ":P": "stuck_out_tongue",
-    ":-P": "stuck_out_tongue",
-    "=P": "stuck_out_tongue",
-    ":-p": "stuck_out_tongue",
-    ":p": "stuck_out_tongue",
-    "=p": "stuck_out_tongue",
-    ":-Þ": "stuck_out_tongue",
-    ":Þ": "stuck_out_tongue",
-    ":þ": "stuck_out_tongue",
-    ":-þ": "stuck_out_tongue",
-    ":-b": "stuck_out_tongue",
-    ":b": "stuck_out_tongue",
-    "d:": "stuck_out_tongue",
-    ":-O": "open_mouth",
-    ":O": "open_mouth",
-    ":-o": "open_mouth",
-    ":o": "open_mouth",
-    ">:O": "open_mouth",
-    ":-X": "no_mouth",
-    ":X": "no_mouth",
-    ":-#": "no_mouth",
-    ":#": "no_mouth",
-    "=X": "no_mouth",
-    "=x": "no_mouth",
-    ":x": "no_mouth",
-    ":-x": "no_mouth",
-    "=#": "no_mouth",
-    "3:)": "smiling_imp",
-    "}:)": "smiling_imp",
-    "}:]": "smiling_imp",
-    ":fish": "fish",
-    ":chicken": "chicken",
-    ":cat": "cat",
-    ":dog": "dog",
-    ":woof": "dog",
-    ":hamster": "hamster",
-    ":mappa": ".sarcasm"
+export const mixerEmojis: Emojis = {
+    ":spaceship": {
+        standard: "rocket"
+    },
+    ":127": {
+        standard: "fire"
+    },
+    ":cactus": {
+        standard: "cactus"
+    },
+    ":whoappa": {
+        standard: "astonished"
+    },
+    ":bacon": {
+        standard: "bacon"
+    },
+    ":beer": {
+        standard: "beer"
+    },
+    ":cake": {
+        standard: "cake",
+        alternatives: ["birthday"]
+    },
+    ":candy": {
+        standard: "candy",
+        alternatives: ["lollipop", "chocolate_bar"]
+    },
+    ":fries": {
+        standard: "fries"
+    },
+    ":icecream": {
+        standard: "ice_cream",
+        alternatives: ["shaved_ice"]
+    },
+    ":pizza": {
+        standard: "pizza"
+    },
+    ":popcorn": {
+        standard: "popcorn"
+    },
+    ":turqmelon": {
+        standard: "watermelon"
+    },
+    ":afk": {
+        standard: "door"
+    },
+    ":controller": {
+        standard: "video_game"
+    },
+    ":delorean": {
+        standard: "blue_car"
+    },
+    ":facepalm": {
+        standard: "person_facepalming"
+    },
+    ":rmf": {
+        standard: "desktop_computer"
+    },
+    ":swag": {
+        standard: "sunglasses"
+    },
+    ":wrench": {
+        standard: "wrench"
+    },
+    ":christmas": {
+        standard: "christmas_tree"
+    },
+    ":jinglebell": {
+        standard: "bell"
+    },
+    "<3": {
+        standard: "heart",
+        alternatives: [
+            "yellow_heart", "green_heart", "blue_heart", "purple_heart",
+            "heartbeat", "heartpulse"
+        ]
+    },
+    "</3": {
+        standard: "broken_heart"
+    },
+    ":')": {
+        standard: "joy"
+    },
+    ":'-)": {
+        standard: "joy"
+    },
+    ":D": {
+        standard: "smiley"
+    },
+    ":-D": {
+        standard: "smiley"
+    },
+    "=D": {
+        standard: "smile"
+    },
+    ":)": {
+        standard: "smile",
+        alternatives: ["smiley", "grin", "grinning"]
+    },
+    ":-)": {
+        standard: "smile",
+        alternatives: ["smiley", "grin", "grinning"]
+    },
+    "=]": {
+        standard: "smile",
+        alternatives: ["smiley", "grin", "grinning"]
+    },
+    "=)": {
+        standard: "smile",
+        alternatives: ["smiley", "grin", "grinning"]
+    },
+    ":]": {
+        standard: "smile",
+        alternatives: ["smiley", "grin", "grinning"]
+    },
+    "\":)": {
+        standard: "sweat_smile"
+    },
+    "\":-)": {
+        standard: "sweat_smile"
+    },
+    "'=)": {
+        standard: "sweat_smile"
+    },
+    "':D": {
+        standard: "sweat_smile"
+    },
+    "':-D": {
+        standard: "sweat_smile"
+    },
+    "'=D": {
+        standard: "sweat_smile"
+    },
+    ">:)": {
+        standard: "satisfied"
+    },
+    ">;)": {
+        standard: "satisfied"
+    },
+    ">:-)": {
+        standard: "satisfied"
+    },
+    ">=)": {
+        standard: "satisfied"
+    },
+    ";)": {
+        standard: "wink"
+    },
+    ";-)": {
+        standard: "wink"
+    },
+    "*-)": {
+        standard: "wink"
+    },
+    "*)": {
+        standard: "wink"
+    },
+    ";-]": {
+        standard: "wink"
+    },
+    ";]": {
+        standard: "wink"
+    },
+    ";^)": {
+        standard: "wink"
+    },
+    "':(": {
+        standard: "sweat"
+    },
+    "':-(": {
+        standard: "sweat"
+    },
+    "'=(": {
+        standard: "sweat"
+    },
+    ":*": {
+        standard: "kissing_heart",
+        alternatives: ["kissing", "kissing_closed_eyes", "kissing_smiling_eyes"]
+    },
+    ":-*": {
+        standard: "kissing_heart",
+        alternatives: ["kissing", "kissing_closed_eyes", "kissing_smiling_eyes"]
+    },
+    "=*": {
+        standard: "kissing_heart",
+        alternatives: ["kissing", "kissing_closed_eyes", "kissing_smiling_eyes"]
+    },
+    ":^*": {
+        standard: "kissing_heart",
+        alternatives: ["kissing", "kissing_closed_eyes", "kissing_smiling_eyes"]
+    },
+    ">:P": {
+        standard: "stuck_out_tongue_winking_eye"
+    },
+    "X-P": {
+        standard: "stuck_out_tongue_winking_eye"
+    },
+    "x-p": {
+        standard: "stuck_out_tongue_winking_eye"
+    },
+    ">:[": {
+        standard: "disappointed"
+    },
+    ":-(": {
+        standard: "disappointed"
+    },
+    ":(": {
+        standard: "disappointed"
+    },
+    ":-[": {
+        standard: "disappointed"
+    },
+    ":[": {
+        standard: "disappointed"
+    },
+    "=(": {
+        standard: "disappointed"
+    },
+    ">:(": {
+        standard: "angry"
+    },
+    ">:-(": {
+        standard: "angry"
+    },
+    ":@": {
+        standard: "angry"
+    },
+    ":'(": {
+        standard: "cry"
+    },
+    ":'-(": {
+        standard: "cry"
+    },
+    ";(": {
+        standard: "cry"
+    },
+    ";-(": {
+        standard: "cry"
+    },
+    ">.<": {
+        standard: "persevere"
+    },
+    ":$": {
+        standard: "flushed"
+    },
+    "=$": {
+        standard: "flushed"
+    },
+    "#-)": {
+        standard: "dizzy_face"
+    },
+    "#)": {
+        standard: "dizzy_face"
+    },
+    "%-)": {
+        standard: "dizzy_face"
+    },
+    "%)": {
+        standard: "dizzy_face"
+    },
+    "X)": {
+        standard: "dizzy_face"
+    },
+    "X-)": {
+        standard: "dizzy_face"
+    },
+    "*\\0/*": {
+        standard: "ok_woman"
+    },
+    "\\0/": {
+        standard: "ok_woman"
+    },
+    "*\\O/*": {
+        standard: "ok_woman"
+    },
+    "\\O/": {
+        standard: "ok_woman"
+    },
+    "O:-)": {
+        standard: "innocent"
+    },
+    "0:-3": {
+        standard: "innocent"
+    },
+    "0:3": {
+        standard: "innocent"
+    },
+    "0:-)": {
+        standard: "innocent"
+    },
+    "0:)": {
+        standard: "innocent"
+    },
+    "0;^)": {
+        standard: "innocent"
+    },
+    "O:)": {
+        standard: "innocent"
+    },
+    "O;-)": {
+        standard: "innocent"
+    },
+    "O=)": {
+        standard: "innocent"
+    },
+    "0;-)": {
+        standard: "innocent"
+    },
+    "O:-3": {
+        standard: "innocent"
+    },
+    "O:3": {
+        standard: "innocent"
+    },
+    "B-)": {
+        standard: "sunglasses"
+    },
+    "B)": {
+        standard: "sunglasses"
+    },
+    "8)": {
+        standard: "sunglasses"
+    },
+    "8-)": {
+        standard: "sunglasses"
+    },
+    "B-D": {
+        standard: "sunglasses"
+    },
+    "8-D": {
+        standard: "sunglasses"
+    },
+    "-_-": {
+        standard: "expressionless"
+    },
+    "-__-": {
+        standard: "expressionless"
+    },
+    "-___-": {
+        standard: "expressionless"
+    },
+    ">:\\": {
+        standard: "confused"
+    },
+    ">:/": {
+        standard: "confused"
+    },
+    ":-/": {
+        standard: "confused"
+    },
+    ":-.": {
+        standard: "confused"
+    },
+    ":/": {
+        standard: "confused"
+    },
+    ":\\": {
+        standard: "confused"
+    },
+    "=/": {
+        standard: "confused"
+    },
+    "=\\": {
+        standard: "confused"
+    },
+    ":L": {
+        standard: "confused"
+    },
+    "=L": {
+        standard: "confused"
+    },
+    ":P": {
+        standard: "stuck_out_tongue"
+    },
+    ":-P": {
+        standard: "stuck_out_tongue"
+    },
+    "=P": {
+        standard: "stuck_out_tongue"
+    },
+    ":-p": {
+        standard: "stuck_out_tongue"
+    },
+    ":p": {
+        standard: "stuck_out_tongue"
+    },
+    "=p": {
+        standard: "stuck_out_tongue"
+    },
+    ":-Þ": {
+        standard: "stuck_out_tongue"
+    },
+    ":Þ": {
+        standard: "stuck_out_tongue"
+    },
+    ":þ": {
+        standard: "stuck_out_tongue"
+    },
+    ":-þ": {
+        standard: "stuck_out_tongue"
+    },
+    ":-b": {
+        standard: "stuck_out_tongue"
+    },
+    ":b": {
+        standard: "stuck_out_tongue"
+    },
+    "d:": {
+        standard: "stuck_out_tongue"
+    },
+    ":-O": {
+        standard: "open_mouth"
+    },
+    ":O": {
+        standard: "open_mouth"
+    },
+    ":-o": {
+        standard: "open_mouth"
+    },
+    ":o": {
+        standard: "open_mouth"
+    },
+    ">:O": {
+        standard: "open_mouth"
+    },
+    ":-X": {
+        standard: "no_mouth"
+    },
+    ":X": {
+        standard: "no_mouth"
+    },
+    ":-#": {
+        standard: "no_mouth"
+    },
+    ":#": {
+        standard: "no_mouth"
+    },
+    "=X": {
+        standard: "no_mouth"
+    },
+    "=x": {
+        standard: "no_mouth"
+    },
+    ":x": {
+        standard: "no_mouth"
+    },
+    ":-x": {
+        standard: "no_mouth"
+    },
+    "=#": {
+        standard: "no_mouth"
+    },
+    "3:)": {
+        standard: "smiling_imp"
+    },
+    "}:)": {
+        standard: "smiling_imp"
+    },
+    "}:]": {
+        standard: "smiling_imp"
+    },
+    ":fish": {
+        standard: "fish"
+    },
+    ":chicken": {
+        standard: "chicken"
+    },
+    ":cat": {
+        standard: "cat"
+    },
+    ":dog": {
+        standard: "dog"
+    },
+    ":woof": {
+        standard: "dog"
+    },
+    ":hamster": {
+        standard: "hamster"
+    },
+    ":mappa": {
+        standard: ".sarcasm"
+    },
 }
