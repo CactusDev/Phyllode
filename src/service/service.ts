361c45274cb90d776dd1f33c8e314713b4563cc7
Innectic <innectic@gmail.com>
2017-08-14 03:07:53 -0700

    Linting

--- src/service/service.ts
+++ src/service/service.ts
@@ -74,29 +74,6 @@ export abstract class Service {
 
     }
 
-    protected reverseEmojis(emojis: Emojis): ReverseEmojis {
-        let reversed: ReverseEmojis = {};
-
-        for (let k of Object.keys(emojis)) {
-            const v = emojis[k];
-            reversed[v.standard] = k;
-        }
-
-        for (let k of Object.keys(emojis)) {
-            const v = emojis[k];
-            if (!v.alternatives) {
-                continue;
-            }
-            for (let alt of v.alternatives) {
-                if (!reversed[alt]) {
-                    reversed[alt] = k;
-                }
-            }
-        }
-
-        return reversed;
-    }
-
     /**
      * Inital connection to the server.
      *
@@ -202,5 +179,28 @@ export abstract class Service {
      */
     public getStatus(): ServiceStatus {
         return this.status;
+    }
+
+    protected reverseEmojis(emojis: Emojis): ReverseEmojis {
+        let reversed: ReverseEmojis = {};
+
+        for (let k of Object.keys(emojis)) {
+            const v = emojis[k];
+            reversed[v.standard] = k;
+        }
+
+        for (let k of Object.keys(emojis)) {
+            const v = emojis[k];
+            if (!v.alternatives) {
+                continue;
+            }
+            for (let alt of v.alternatives) {
+                if (!reversed[alt]) {
+                    reversed[alt] = k;
+                }
+            }
+        }
+
+        return reversed;
     }
 }
